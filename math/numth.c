56135ad7258b14fd92916498ebec142984edf6e8
wcc <wcc@localhost.localdomain>
2016-09-05 17:13:20 +0800

    gcd_e() & mle() 	modified:   math/numth.c 	modified:   math/numth_test.c

--- math/numth.c
+++ math/numth.c
@@ -5,13 +5,56 @@
 #ifndef _TJYTLXWXHYZQFW_NUMTH_C
 #define _TJYTLXWXHYZQFW_NUMTH_C
 
+#define NUMTH_CORRECT(x, mod) ((x) += ((x) < 0 ? (mod) : 0))
+
+#define NUMTH_ENS (-1) /* error: no solution */
+
 #include <assert.h>
 
 /* a^b % n */
 long long numth_modexp(long long a, long long b, long long n);
+
+/* gcd(|a|, |b|) , gcd(0, 0) = 0 */
 long long numth_gcd(long long a, long long b);
 
+/* *d = |a| * x + |b| * y , 0 = 0*0 + 0*0 */
+void numth_gcd_e(long long a, long long b, long long *d, long long *x,
+	long long *y);
+
+/*
+ * Modular linear equation solver
+ * @ia534
+ *
+ * |a|*x = |b| (mod |n|), there are gcd(|a|, |n|) solutions,
+ * this function returns just one of them.
+ *
+ * Parameters:
+ * 	n:**positive** number
+ * 	delta: = n / gcd(a, n)
+ * 	solution: st. |a|*x = |b| (mod n), if a < 0, b > 0 
+ * 		then -x is a solution of ax=b(mod n)
+ * 	
+ */
+int numth_mle(long long a, long long b, long long n, long long *x,
+	long long *delta);
+
+/*
+ * @ia534
+ *
+ * This ineffictive as there is a 'n/delta'.
+ *
+ * Important:
+ * 	1. i & x must be variables.
+ * 	2. ensure that the equation dose have solutions!
+ */
+#define numth_for_each_solution(i, x, n, delta) \
+	for ((i) = 0; (i) < (n) / (delta); \
+		(i) += 1, (x) = ((x) + (delta)) % (n))
+	
+
 static long long numth_do_gcd(long long greater, long long lesser);
+static void numth_do_gcd_e(long long greater, long long lesser,
+	long long *d, long long *x, long long *y);
 
 long long numth_modexp(long long a, long long b, long long n)
 {
@@ -56,6 +99,67 @@ static long long numth_do_gcd(long long a, long long b)
 	}
 
 	return b;
+}
+
+void numth_gcd_e(long long a, long long b, long long *d,
+	long long *x, long long *y)
+{
+	if (a == 0 && b == 0) {
+		*d = *x = *y = 0;
+		return;
+	}
+
+	if (a < 0)
+		a *= -1;
+	if (b < 0)
+		b *= -1;
+
+	if (a < b)
+		numth_do_gcd_e(b, a, d, y, x);
+	else
+		numth_do_gcd_e(a, b, d, x, y);
+}
+
+static void numth_do_gcd_e(long long a, long long b,
+	long long *d, long long *x, long long *y)
+{
+	long long tmp;
+
+	if (b == 0) {
+		*d = a;
+		*x = 1;
+		*y = 0;
+		return;
+	}
+	numth_do_gcd_e(b, a % b, d, x, y);
+	tmp = *x;
+	*x = *y;
+	//TODO: overflow?
+	*y = (tmp - (a / b) * (*y));
+}
+
+int numth_mle(long long a, long long b, long long n, long long *solution,
+	long long *delta)
+{
+	long long d, x, y;
+
+	assert(n != 0);
+
+	if (n < 0)
+		n *= (-1);
+	if (a < 0)
+		a *= (-1);
+	if (b < 0)
+		b *= (-1);
+
+	numth_gcd_e(a, n, &d, &x, &y);
+	assert(d != 0); //d=0 if and only if a=b=0
+	if (b % d)
+		return NUMTH_ENS;
+	*solution = ((b / d) * (x % n)) % n;
+	NUMTH_CORRECT(*solution, n);
+	*delta = n / d;
+	return 0;
 }
 
 #endif
--- math/numth_test.c
+++ math/numth_test.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "/home/wcc/OnlineJudge/source/common.h"
+
 #include "numth.c"
 
 int test_modexp(void)
@@ -16,12 +18,42 @@ int test_modexp(void)
 	return 0;
 }
 
-int main(void)
+int test_gcd(void)
 {
 	long long a, b;
 
 	while (scanf("%lld%lld", &a, &b) == 2)
 		printf("%lld\n", numth_gcd(a, b));
+
+	return 0;
+}
+
+int test_gcd_e(void)
+{
+	long long a, b, d, x, y;
+
+	while (scanf("%lld%lld", &a, &b) == 2) {
+		numth_gcd_e(a, b, &d, &x, &y);
+		printf("%lld: %lldx(%lld)+%lldx(%lld)\n", 
+			d, a, x, b, y);
+	}
+
+	return 0;
+}
+
+int main(void)
+{
+	long long a, b, n, x, delta;
+	long long i;
+
+	while (scanf("%lld%lld%lld", &a, &b, &n) == 3) {
+		if (numth_mle(a, b, n, &x, &delta) == 0) {
+			numth_for_each_solution(i, x, ABS(n), delta)
+				printf("%lld\n", x);
+		} else {
+			printf("no solution\n");
+		}
+	}
 
 	return 0;
 }
