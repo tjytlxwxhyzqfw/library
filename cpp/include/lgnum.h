5f1e2fdf81cc249924f6f443ba576395aaa89a5e
wcc <wcc@localhost.localdomain>
2016-10-19 17:49:53 +0800

    + lgnum.h 	modified:   cpp/Makefile 	new file:   cpp/include/lgnum.h 	new file:   cpp/lib/liblgnum.so 	new file:   cpp/src/math/lgnum.cpp 	modified:   scripts/source.pyc

---
+++ cpp/include/lgnum.h
@@ -0,0 +1,97 @@
+/**
+ * @author wcc
+ * 2016-10-19
+ */
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+template <int width> struct lgnum {
+	static int strpls(const char *x, const char *y, char *r) {
+		int i, xlen, ylen;
+		int tres, carry;
+
+		xlen = strlen(x), ylen = strlen(y);
+		carry = 0;
+		for (i = 0; i < xlen && i < ylen; ++i) {
+			tres = carry + x[i] + y[i] - 96;
+			carry = tres / 10;
+			r[i] = tres % 10 + 48;
+		}
+
+		for (; i < xlen; ++i) {
+			tres = carry + x[i] - 48;
+			carry = tres / 10;
+			r[i] = tres % 10 + 48;
+		}
+
+		for (; i < ylen; ++i) {
+			tres = carry + y[i] - 48;
+			carry = tres / 10;
+			r[i] = tres % 10 + 48;
+		}
+
+		if (carry != 0) r[i++] = '1';
+		r[i] = 0;
+
+		return i;
+	}
+
+	static void pls(const lgnum &x, const lgnum &y, lgnum &r) {
+		r.len = strpls(x.num, y.num, r.num);
+	}
+
+	char *num;
+	int len;
+
+	lgnum(void):num(NULL), len(0) {
+		num = new char[width+1];
+	}
+
+	void setv(const char *str) {
+		int i, m;
+		len = strlen(str);
+		std::copy(str, str+len+1, num);
+		m = (len-1) / 2;
+		for (i = 0; i <= m; ++i)
+			std::swap(num[i], num[len-1-i]);
+	}
+
+	inline bool operator==(const lgnum &rival) const {
+		if (len != rival.len) return false;
+		return strcmp(num, rival.num) == 0;
+	}
+
+	bool operator <(const lgnum &rival) const {
+		if (len == rival.len) {
+			char *s = num+len, *t = rival.num+len;
+			while (s > num && (*s == *t)) {
+				s--, t--;
+			};
+			return *s < *t;
+		}
+		return len < rival.len;
+	}
+
+	inline bool operator>(const lgnum &rival) const {
+		return rival < *this;
+	}
+
+	inline bool operator>=(const lgnum &rival) const {
+		return !(*this < rival);
+	}
+
+	inline bool operator<=(const lgnum &rival) const {
+		return !(*this > rival);
+	}
+
+	void print(void) const {
+		int len, i;
+
+		len = strlen(num);
+		for (i = len - 1; i >= 0; --i)
+			printf("%c", num[i]);
+		printf("\n");
+	}
+};
